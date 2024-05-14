620d116dfb05ed8cb5d36d476b859d4d923a19b5
Nicole M <nicolem@MacBook-Pro-de-Nicole.local>
2024-05-14 13:17:16 -0500

    lambdafunction update

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,17 @@
-import pandas as pd
-
+import json
 
 def lambda_handler(event, context):
-   d = {'col1': [1,2], 'col2': [3,4]} 
-   df = pd.DataFrame(data=d)
-   print(df)
-   print('Done x1.8')
+    x = 5
+    y = 10
+
+    def suma(a, b):
+        operacion = a + b
+        return operacion
+
+    resultado = suma(x, y)
+
+    # Returning the result as a JSON object
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'resultado': resultado})
+    }
